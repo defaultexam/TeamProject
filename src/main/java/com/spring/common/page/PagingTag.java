1fef2f27369500234aa35bd8fb3a7f2d7153c792
taeyoub kim <kty1331@hanmail.net>
2018-01-24 13:17:17 +0900

    boardList.jsp,사진들,pagingtag 수정 글번호정렬, 페이징 이모티콘 설정완료

--- src/main/java/com/spring/common/page/PagingTag.java
+++ src/main/java/com/spring/common/page/PagingTag.java
@@ -35,60 +35,6 @@ public class PagingTag extends TagSupport {
 		return super.doStartTag();
 	}
 
-	public String getPaging() {
-		String ret = "";
-		if (page < 1) {
-			page = 1;
-		}
-		if (total < 1) {
-			return "";
-		}
-		int currentFirst = ((page - 1) / page_size) * page_size + 1;
-		int currentlast = ((page - 1) / page_size) * page_size + page_size;
-		int nextFirst = (((page - 1) / page_size) + 1) * page_size + 1;
-		int prevFirst = (((page - 1) / page_size) - 1) * page_size + 1;
-		int lastPage = 1;
-		lastPage = total / list_size;
-		if (total % list_size != 0)
-			lastPage = lastPage + 1;
-		currentlast = (currentlast > lastPage) ? lastPage : currentlast;
-		ret += " <div class='paginate'> ";
-		if (page > 1) {
-			ret += "<a href=\"javascript:goPage('1')\"><span><img src='/resources/images/common/btn_paginate_first.gif' alt='처음' /></span></a>";
-		} else {
-			ret += " <span><img src='/resources/images/common/ btn_paginate_first.gif' alt='처음' /></span> ";
-		}
-		if (prevFirst > 0) {
-			ret += " <a href=\"javascript:goPage('" + prevFirst
-					+ "');\"><span> <img src='/resources/images/common/btn_paginate_prev.gif'    alt='이전' /> </span></a> ";
-		} else {
-			ret += " <span><img src='/resources/images/common/ btn_paginate_prev.gif'    alt='이전' /></span> ";
-		}
-		for (int j = currentFirst; j < currentFirst + page_size && j <= lastPage; j++) {
-			if (j <= currentlast) {
-				if (j == page) {
-					ret += " <a href='#' class='on textAn'>" + j + "</a> ";
-				} else {
-					ret += " <a href=\"javascript:goPage('" + j + "');\"  class='textAn'>" + j + "</a> ";
-				}
-			}
-		}
-		if (nextFirst <= lastPage) {
-			ret += " <a href=\"javascript:goPage('" + nextFirst
-					+ "')\"><span> <img src='/resources/images/common/btn_paginate_next.gif' alt='다음' /> </span></a> ";
-		} else {
-			ret += " <span><img src='/resources/images/common/ btn_paginate_next.gif' alt='다음' /></span> ";
-		}
-		if (page < lastPage) {
-			ret += " <a href=\"javascript:goPage('" + lastPage
-					+ "')\"><span> <img src='/resources/images/common/btn_paginate_last.gif' alt='마지막' /> </span></a> ";
-		} else {
-			ret += " <span><img src='/resources/images/common/btn_paginate_last.gif' alt='마지막' /></span> ";
-		}
-		ret += " </div> ";
-		return ret;
-	}
-
 	public void setPage(int page) {
 		this.page = page;
 	}
@@ -103,6 +49,67 @@ public class PagingTag extends TagSupport {
 
 	public void setPage_size(int page_size) {
 		this.page_size = page_size;
+	}
+
+	public String getPaging() {
+		String ret = "";
+		if (page < 1) {
+			page = 1;
+		}
+		if (total < 1) {
+			return "";
+		}
+		int currentFirst = ((page - 1) / page_size) * page_size + 1;
+		int currentlast = ((page - 1) / page_size) * page_size + page_size;
+		int nextFirst = (((page - 1) / page_size) + 1) * page_size + 1;
+		int prevFirst = (((page - 1) / page_size) - 1) * page_size + 1;
+
+		int lastPage = 1;
+		lastPage = total / list_size;
+
+		if (total % list_size != 0)
+			lastPage = lastPage + 1;
+		currentlast = (currentlast > lastPage) ? lastPage : currentlast;
+		
+		ret += "<div class='paginate'>";
+		
+		if (page > 1) {
+			ret += "<a href=\"javascript:goPage('1')\"><span><img src='/resources/images/common/btn_paginate_first.gif' alt='처음' /></span></a>";
+		} else {
+			ret += "<span><img src='/resources/images/common/btn_paginate_first.gif' alt='처음' /></span>";
+		}
+		
+		if (prevFirst > 0) {
+			ret += " <a href=\"javascript:goPage('" + prevFirst
+					+ "');\"><span><img src='/resources/images/common/btn_paginate_prev.gif' alt='이전' /></span></a> ";
+		} else {
+			ret += " <span><img src='/resources/images/common/btn_paginate_prev.gif' alt='이전' /></span> ";
+		}
+		
+		for (int j = currentFirst; j < currentFirst + page_size && j <= lastPage; j++) {
+			if (j <= currentlast) {
+				if (j == page) {
+					ret += " <a href='#' class='on textAn'>" + j + "</a> ";
+				} else {
+					ret += " <a href=\"javascript:goPage('" + j + "');\" class='textAn'>" + j + "</a> ";
+				}
+			}
+		}
+		
+		if (nextFirst <= lastPage) {
+			ret += " <a href=\"javascript:goPage('" + nextFirst
+					+ "')\"><span><img src='/resources/images/common/btn_paginate_next.gif' alt='다음' /></span></a> ";
+		} else {
+			ret += " <span><img src='/resources/images/common/btn_paginate_next.gif' alt='다음' /></span> ";
+		}
+		if (page < lastPage) {
+			ret += " <a href=\"javascript:goPage('" + lastPage
+					+ "')\"><span> <img src='/resources/images/common/btn_paginate_last.gif' alt='마지막' /> </span></a> ";
+		} else {
+			ret += " <span><img src='/resources/images/common/btn_paginate_last.gif' alt='마지막' /></span> ";
+		}
+		ret += " </div> ";
+		return ret;
 	}
 
 }
